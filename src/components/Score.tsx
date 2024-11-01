101d6d55378cee23bf44ecc91af4d6cd0def6b96
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-11-01 17:52:53 +0100

    - updated score text color

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -6,9 +6,9 @@ type ScoreProps = {
 };
 
 const Score: React.FC<ScoreProps> = ({ score, skips }) => (
-   <div className="flex justify-between text-lg font-semibold text-gray-700 mb-4">
-      <p>Score: <span className="text-green-500">{score}</span></p>
-      <p>Skips left: <span className="text-red-500">{skips}</span></p>
+   <div className="flex justify-between text-lg font-semibold text-zinc-100 mb-4">
+      <p>Score: <span className="text-green-300">{score}</span></p>
+      <p>Skips left: <span className="text-red-300">{skips}</span></p>
    </div>
 );
 
